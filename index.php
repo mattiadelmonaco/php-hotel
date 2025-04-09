ef883d9b4aa1b247db31304b4e52ff3e23e5f8bf
mattiadelmonaco <mattia.delmonaco@gmail.com>
2025-04-09 17:05:35 +0200

    feat: add table with bootstrap

--- index.php
+++ index.php
@@ -52,20 +52,21 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
 <!-- /BOOTSTRAP -->
 </head>
-<body>
+<body class="bg-dark-subtle">
     <!-- HEADER -->
-    <header><h1>LISTA HOTEL</h1></header>
+    <header><h1 class="text-center my-3">LISTA HOTEL</h1></header>
     <!-- /HEADER -->
 <!-- MAIN -->
     <main>
-    <?php
-    foreach ($hotels as $hotel) {
 
-        if ($hotel["parking"] === true) {
-            $hotel["parking"] = "Disponibile";
-        } else {
-            $hotel["parking"] = "Non disponibile";
-        }
+     <?php
+    // foreach ($hotels as $hotel) {
+
+    //     if ($hotel["parking"] === true) {
+    //         $hotel["parking"] = "Disponibile";
+    //     } else {
+    //         $hotel["parking"] = "Non disponibile";
+    //     }
 
         // stampare in pagina manualmente i dati di tutti gli hotel
 
@@ -78,15 +79,43 @@
 
         // stampare in pagina i dati di tutti gli hotel con un altro foreach
 
-        foreach ($hotel as $key => $value) {
-            echo "<b>" . $key . "</b>" . ": " . $value . "<br>";
-        }
-        echo "<br>";
-        
-    }
-
-    
+        // foreach ($hotel as $key => $value) {
+        //     echo "<b>" . $key . "</b>" . ": " . $value . "<br>";
+        // }
+        // echo "<br>";
+    // }
     ?>
+<section class="container">
+<table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Descrizione</th>
+                    <th>Parcheggio</th>
+                    <th>Voto</th>
+                    <th>Distanza dal centro</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($hotels as $hotel) {
+                    if ($hotel["parking"] === true) {
+                        $hotel["parking"] = "Disponibile";
+                    } else {
+                        $hotel["parking"] = "Non disponibile";
+                    }
+                    echo "<tr>";
+                    echo "<td>" . $hotel['name'] . "</td>";
+                    echo "<td>" . $hotel['description'] . "</td>";
+                    echo "<td>" . $hotel['parking'] . "</td>";
+                    echo "<td>" . $hotel['vote'] . "</td>";
+                    echo "<td>" . $hotel['distance_to_center'] . " km</td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+        </section>
     </main>
 <!-- /MAIN -->
 
